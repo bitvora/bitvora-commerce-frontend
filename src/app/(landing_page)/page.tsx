277be71ec4de47dbaba3f8821e59f1536537398f
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2025-05-09 06:41:17 +0100

    Fix hero

--- src/app/(landing_page)/page.tsx
+++ src/app/(landing_page)/page.tsx
@@ -8,7 +8,7 @@ import { PrimaryButton, SecondaryButton } from '@/components/Buttons';
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-bg flex flex-col">
+    <div className="w-screen h-screen bg-bg flex flex-col overflow-y-auto overflow-x-hidden">
       <header className="w-full fixed top-6 md:top-4 left-0 right-0 px-5 md:px-0">
         <div
           className="rounded-4xl xl:rounded-[4000px] h-16 md:h-[64px] lg:h-[80px] w-full md:max-w-[600px] z-[1000] bg-bg lg:max-w-[900px] xl:max-w-[1050px] 2xl:max-w-[1256px] mx-auto border-[0.5px] border-primary-500 px-[20px] xl:px-[32px] py-[px] flex items-center justify-between"
@@ -34,23 +34,25 @@ export default function Home() {
 
       <div
         id="hero"
-        className="flex items-center gap-8 w-full py-[20px] sm:py-[20px] lg:py-[300px] pl-6 sm:pl-8 md:pl-[50px] lg:pl-[75px] xl:pl-[100px] 2xl:pl-[160px]">
-        <div className="w-full flex flex-col gap-4 max-w-[500px]">
+        className="flex items-center gap-8 w-full py-[200px] sm:py-[250px] lg:py-[300px] pl-10 sm:pl-8 md:pl-[50px] lg:pl-[75px] xl:pl-[100px] 2xl:pl-[160px] pr-10 lg:pr-0">
+        <div className="w-full flex flex-col gap-4 lg:max-w-[500px] text-center md:text-start mx-auto md:mx-[unset]">
           <RegularHeader3>
             Open Source <span className="text-secondary-700">Bitcoin Payments</span> For Everyone
           </RegularHeader3>
 
-          <SemiboldBody className="text-light-700">
+          <SemiboldBody className="text-light-700 justify-center">
             The 100% open source Bitcoin payment platform for merchants, online stores, and
             businesses. Accept Lightning and on-chain payments without giving up control.
           </SemiboldBody>
 
-          <div className="flex items-center gap-4 mt-4">
-            <Link href={app_routes.signup}>
-              <PrimaryButton className="h-14 min-w-[240px]">Get Started for Free</PrimaryButton>
+          <div className="flex flex-col sm:flex-row items-center gap-4 mt-4 w-full">
+            <Link href={app_routes.signup} className="w-full">
+              <PrimaryButton className="h-14 min-w-[240px] w-full">
+                Get Started for Free
+              </PrimaryButton>
             </Link>
 
-            <SecondaryButton className="h-14 min-w-[172px]">Self-Host It</SecondaryButton>
+            <SecondaryButton className="h-14 min-w-[172px] w-full">Self-Host It</SecondaryButton>
           </div>
         </div>
       </div>
